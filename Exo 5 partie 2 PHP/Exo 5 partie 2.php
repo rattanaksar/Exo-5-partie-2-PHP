81307283757fe8d2338c38996c08b80e57194ae0
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 13:17:15 +0200

    Exo 5 if et des else

---
+++ Exo 5 partie 2 PHP/Exo 5 partie 2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 5 : Les conditions</title>
+</head>
+<body>
+    <h1>Exo 5 : Les conditions</h1>
+    <h4>Traduire ce code avec des if et des else : </h4>
+    <?php
+    $gender = "Femme";
+
+    if ($gender === "Femme") {
+        echo"C'est une développeuse";
+    } else {
+        echo"C'est un développeur";
+    }
+    ?>
+</body>
+</html>
