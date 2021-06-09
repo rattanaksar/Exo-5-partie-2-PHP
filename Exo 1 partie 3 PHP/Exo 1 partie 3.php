ae128308355891414ba7f8a1251b061d7292c011
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 15:59:22 +0200

    first commit

---
+++ Exo 1 partie 3 PHP/Exo 1 partie 3.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 1 : Les boucles</title>
+</head>
+<body>
+    <h1>Exo 1 : Les boucles</h1>
+    <h2>Exo 1</h2>
+    <p>
+        Créer une variable et l'initialiser à 0. Tant que cette variable n'atteint pas 10, il faut :
+        - l'afficher
+        - l'incrementer
+    </p>
+    <?php
+    for ($i = 0; $i <= 10; $i++){
+        echo $i;
+    }
+    ?>
+    </p>
+</body>
+</html>
