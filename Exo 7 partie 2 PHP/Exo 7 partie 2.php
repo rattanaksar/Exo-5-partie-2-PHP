5a598bb672072c4cc78261d8e692392f2d68fbcc
rattanaksar <81554110+rattanaksar@users.noreply.github.com>
2021-06-09 15:36:59 +0200

    Rajout balise P

---
+++ Exo 7 partie 2 PHP/Exo 7 partie 2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exo 7 : Les conditions</title>
+</head>
+<body>
+    <h1>Exo 7 : Les conditions</h1>
+    <h4>Traduire ce code avec des if et des else : </h4>
+    <p>
+    <?php
+    $isOK = true;
+    if ($isOK === true)
+      {
+          echo 'C\'est ok';
+      }
+      elseif ($isOK === false)
+      {
+          echo 'C\'est pas bon';
+      }
+    ?>
+    </p>
+</body>
+</html>
